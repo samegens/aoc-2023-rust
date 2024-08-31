4e32d58c2cb38bc820f1bce13ec4d76ae651c69a
Sebastiaan Megens <sebastiaan@blauwe-lucht.nl>
2024-08-31 06:26:54 +0200

    Added: point struct.

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -3,3 +3,6 @@ pub use input_reader::InputReader;
 
 mod grid;
 pub use grid::Grid;
+
+mod point;
+pub use point::Point;
---
+++ common/src/point.rs
@@ -0,0 +1,72 @@
+#[derive(Debug, PartialEq)]
+pub struct Point {
+    pub x: i64,
+    pub y: i64,
+}
+
+impl Point {
+    pub fn new(x: i64, y: i64) -> Self {
+        Point { x, y }
+    }
+
+    pub fn translate(&self, dx: i64, dy: i64) -> Self {
+        Point::new(self.x + dx, self.y + dy)
+    }
+
+    pub fn distance_to(&self, other: &Point) -> f64 {
+        let dx = (other.x - self.x) as f64;
+        let dy = (other.y - self.y) as f64;
+        (dx.powi(2) + dy.powi(2)).sqrt()
+    }
+
+    pub fn manhattan_distance(&self, other: &Point) -> u64 {
+        (self.x - other.x).unsigned_abs() + (self.y - other.y).unsigned_abs()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_translate() {
+        // Arrange
+        let point = Point::new(3, 4);
+        let expected = Point::new(5, 7);
+
+        // Act
+
+        let actual = point.translate(2, 3);
+
+        // Assert
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_distance_to() {
+        // Arrange
+        let point1 = Point::new(1, 2);
+        let point2 = Point::new(4, 6);
+        let expected = 5.0;
+
+        // Act
+        let actual = point1.distance_to(&point2);
+
+        // Assert
+        assert!((actual - expected).abs() < f64::EPSILON);
+    }
+
+    #[test]
+    fn test_manhattan_distance() {
+        // Arrange
+        let point1 = Point::new(1, 2);
+        let point2 = Point::new(4, 6);
+        let expected = 7;
+
+        // Act
+        let actual = point1.manhattan_distance(&point2);
+
+        // Assert
+        assert_eq!(actual, expected);
+    }
+}
